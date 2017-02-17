735d6ae15dfdf822eb9caeec1d7289079955f754
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-17 14:19:29 -0600

    drill 1 complete

---
+++ 020-ajax-json/drill_1/public/scripts.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function (){
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', "people", true);
+	if (onloadfunction != undefined) {
+		ourRequest.onload = function() {
+			var result = ourRequest.responseText;
+			onloadfunction(result);
+		}
+	}
+	ourRequest.send();
+
+	function onloadfunction(result) {
+		data = JSON.parse(result);
+		createHTML(data);
+		document.getElementsByTagName('body')[0].innerHTML = htmlToInsert;
+	}
+
+	function createHTML(data){
+		htmlToInsert = "<h1>People Information List</h1>";
+		for (var i=0;i<data.length;i++) {
+			htmlToInsert += "<div><h3>Person #" + parseInt(i + 1) + "</h3>";
+			htmlToInsert += "<p>First name: " + data[i]["fname"] + "</p>";
+			htmlToInsert += "<p>Last name: " + data[i]["lname"] + "</p>";
+
+			htmlToInsert += "<p>Address: " + data[i]["address"] + "</p>";
+			htmlToInsert += "<p>           " + data[i]["city"] + ", " + data[i]["state"]
+
+			htmlToInsert += " " + data[i]["zip"] + "</p>";
+			htmlToInsert += "<p>Telephone number: " + data[i]["tel"] + "</p></div>";
+		}
+	}
+
+
+});
+
